0936cfb85caa1a9581414caf5f9b063dadf52d1b
Luis David Ocampo Aguirre <luisda199824@gmail.com>
2018-04-23 13:48:28 -0500

    Generacion de secciones

--- BodaKaoz/Fiesta/views.py
+++ BodaKaoz/Fiesta/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 
 def fiestaDashboardView(request):
-	template = get_template('base/base.html')
+	template = get_template('Fiesta/fiesta.html')
 	context = {}
 	return HttpResponse(template.render(context, request))
