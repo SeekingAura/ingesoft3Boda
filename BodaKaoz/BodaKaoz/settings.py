71760c51bb33daf096dff040a8be933037bc02ce
sebastian <shuertas@utp.edu.co>
2018-04-25 12:50:59 -0500

    ejemplo bootstrapp
    manejo sencillo de bootstrapp como pequeño ejemplo del iniciar sesion

--- BodaKaoz/BodaKaoz/settings.py
+++ BodaKaoz/BodaKaoz/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'BodaKaoz.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
-
+}
+'''
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
@@ -95,7 +95,7 @@ DATABASES = {
          'PORT': '5432',
      }
  }
-
+'''
 
 
 # Password validation
