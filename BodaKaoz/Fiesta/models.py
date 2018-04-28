90cceeb3066bb691e6f040f2f1429f954dff0b42
Carlos Arturo <carlox216@utp.edu.co>
2018-04-28 13:50:00 -0500

    Revert "Update models - precio field"
    This reverts commit 5898f8f664a7c53a2b689383f7f296b802e92fc4.

--- BodaKaoz/Fiesta/models.py
+++ BodaKaoz/Fiesta/models.py
@@ -7,24 +7,23 @@ class FiestaEvento(models.Model):
 	Boda=models.ForeignKey(Boda, on_delete=models.CASCADE)
 	Lugar=models.ForeignKey(Lugar, on_delete=models.SET_NULL, null=True, blank=True, default=None)
 	Fotos=models.ForeignKey(Fotos, on_delete=models.SET_NULL, null=True, blank=True, default=None)
-	precio=models.BigIntegerField(default=0)
+	precio=models.IntegerField(default=0)
 		
 class Alimento(models.Model):
 	nombre=models.CharField(max_length=50)
 	descripcion=models.CharField(max_length=50)
 	imagen=models.ImageField(null=True, blank=True, default=None)
-	precio=models.BigIntegerField(default=0)
+	precio=models.IntegerField(default=0)
 		
 class AlimentoCarrito(models.Model):
 	FiestaEvento=models.ForeignKey(FiestaEvento, on_delete=models.CASCADE)
 	Alimento=models.ForeignKey(Alimento, on_delete=models.CASCADE)
-	cantidad=models.IntegerField(default=0)
 		
 class Entretenimiento(models.Model):
 	nombre=models.CharField(max_length=50)
 	descripcion=models.CharField(max_length=50)
 	imagen=models.ImageField(null=True, blank=True, default=None)
-	precio=models.BigIntegerField(default=0)
+	precio=models.IntegerField(default=0)
 
 class EntretenimientoCarrito(models.Model):
 	FiestaEvento=models.ForeignKey(FiestaEvento, on_delete=models.CASCADE)
@@ -34,7 +33,7 @@ class DecoracionFiesta(models.Model):
 	nombre=models.CharField(max_length=50, null=True, blank=True, default=None)
 	descripcion=models.CharField(max_length=50, null=True, blank=True, default=None)
 	imagen=models.ImageField(null=True, blank=True, default=None)
-	precio=models.BigIntegerField(default=0)
+	precio=models.IntegerField(default=0)
 	class Meta:
 		verbose_name = "Decoración"
 		verbose_name_plural = "Decoraciones"
