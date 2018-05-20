c3fbf2fbfbc08178e1d49582f8a7c229bce5db27
Carlos Arturo <carlox216@utp.edu.co>
2018-05-20 14:46:26 -0500

    Fix redirect when alredy authenticated with admin user
    Admin user get error's when try to get in to web page, but redirect infinite times because admin user dont have Boda and Enamorado objects
    
    if user is_staff is True logout

--- BodaKaoz/BodaKaoz/views.py
+++ BodaKaoz/BodaKaoz/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import redirect
+from django.contrib.auth import logout
 
 def handler404View(request):
+    if(request.user is not None):
+        if(request.user.is_staff):
+            logout(request)
     return redirect("index")
